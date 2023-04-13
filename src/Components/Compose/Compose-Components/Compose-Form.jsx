42920943e0e57b7e440f46fd1aa489500cc88b39
Yuxiao Yan <100373846+taysgithub@users.noreply.github.com>
2023-04-13 13:16:18 -0400

    4th-commit-13Apr23-2nd

--- src/Components/Compose/Compose-Components/Compose-Form.jsx
+++ src/Components/Compose/Compose-Components/Compose-Form.jsx
@@ -6,7 +6,8 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
-
+// Scss
+import "./Compose-Form.scss";
 // Hook
 import useAuth from "../../../Hook/useAuth";
 import useCompose from "../../../Hook/useCompose";
